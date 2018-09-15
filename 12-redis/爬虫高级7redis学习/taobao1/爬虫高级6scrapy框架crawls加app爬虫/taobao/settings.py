2550d288cbe3a8cd7924d6cdb8a4cb62382d7de4
zhongxin <490336534@qq.com>
2018-09-15 17:21:35 +0800

    调整文件位置

---
+++ 12-redis/爬虫高级7redis学习/taobao1/爬虫高级6scrapy框架crawls加app爬虫/taobao/settings.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for taobao project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'taobao'
+
+SPIDER_MODULES = ['taobao.spiders']
+NEWSPIDER_MODULE = 'taobao.spiders'
+
+# 搜索的关键字
+KEY_WORDS = '女装 裙'
+# 爬取多少页的数据，1 到 100 之间
+PAGE_NUM = 10
+# 每页显示的商品数
+ONE_PAGE_COUNT = 44
+
+LOG_LEVEL = 'ERROR'
+
+# 定时启动
+START_TIME = '2018-04-07 21:50:30'
+STRUCT_TIME = '%Y-%m-%d %H:%M:%S'
+
+# 数据库配置
+MONGO_URI = 'mongodb://admin:admin@localhost:27017'
+MONGO_DATABASE = 'taobao'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'taobao (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+ITEM_PIPELINES = {
+   'taobao.pipelines.TaobaoHandlePipeline': 200,
+   # 'taobao.pipelines.TaobaoHandle2PipeLine': 210, # 另外的功能pipeline
+   'taobao.pipelines.TaobaoPipeline': 300,
+}
+
+# 设置所有 request 都生效的默认的 headers
+DEFAULT_REQUEST_HEADERS = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+    'Upgrade-Insecure-Requests': '1',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.9'
+}
+
+# 随机的 user-agent 列表
+# 只有 63 是有效的，其他的2个都是随便设置的，大家项目中一定要设置正确的 user-agent
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.3239.132 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3239.132 Safari/537.36',
+]
+
+# 设置下载中间件
+DOWNLOADER_MIDDLEWARES = {
+   # 'taobao.middlewares.TaobaoDownloaderMiddleware': 543,
+   'taobao.middlewares.UserAgentDownloaderMiddleware': 400,
+   # 'taobao.middlewares.ProxyDownloaderMiddleware': 500,
+}
+
+
+# 配置 代理
+PROXY_LOCAL = '127.0.0.1:8888'
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'taobao.middlewares.TaobaoSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'taobao.pipelines.TaobaoPipeline': 300,
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
