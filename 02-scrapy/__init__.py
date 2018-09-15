1f1ccf26c8c3223276752501cb09c15a1cc9b02b
zhongxin <490336534@qq.com>
2018-09-15 17:12:40 +0800

    调整文件位置

---
+++ 02-scrapy/__init__.py
@@ -0,0 +1,3 @@
+#coding=utf-8
+#Auth:zx
+#Time:2018/9/15 0015 17:00
--- 14-scrapy-redis/爬虫高级9scrapy-redis学习/h9/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-__author__ = 'Terry'
-
-from scrapy_redis.scheduler import Scheduler
-
-from scrapy_redis.dupefilter import RFPDupeFilter
